92e373b5eaac7c204e3c5fa668ffa1cbfaef8ce5
anh-tv <anh.tv.dev.vn@gmail.com>
2024-02-13 23:06:30 +0700

    Clean code

--- guaranteed_rows.py
+++ guaranteed_rows.py
@@ -4,5 +4,5 @@ with open("rows.txt", "r") as f:
     data = f.readlines()
     for line in data:
         tomography = list(map(int, line.strip().split(",")))
-        print("".join(map(lambda x: "o" if x else "x", get_guaranteed(20, tomography))))
-
+        result = get_guaranteed(20, tomography)
+        print("".join(map(lambda x: "o" if x else "x", result)))
